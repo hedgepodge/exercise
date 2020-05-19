c47f8005535e0ca3ceabb98fcdb1cc320631180e
Hedgepodge <45145360+hedgepodge@users.noreply.github.com>
2020-05-19 09:55:50 +0900

    Create ch2.py

---
+++ algo_2020.03/ch2.py
@@ -0,0 +1,49 @@
+########## Linear Search 1 ##########
+
+def linear_search(element, some_list):
+    for i in range(len(some_list)):
+        if some_list[i] == element:
+            return(i)
+    return None
+
+print(linear_search(2, [2, 3, 5, 7, 11]))
+print(linear_search(0, [2, 3, 5, 7, 11]))
+print(linear_search(5, [2, 3, 5, 7, 11]))
+print(linear_search(3, [2, 3, 5, 7, 11]))
+print(linear_search(11, [2, 3, 5, 7, 11]))
+
+
+########## Binary Search 1 ##########
+def binary_search(element, some_list):
+    start = 0
+    end = len(some_list) - 1
+    while (start <= end):
+        a = (start + end) // 2
+        if some_list[a] == element:
+            return (a)
+        elif some_list[a] < element:
+            start = a + 1
+        else:
+            end = a - 1
+    return None
+
+print(binary_search(2, [2, 3, 5, 7, 11]))
+print(binary_search(0, [2, 3, 5, 7, 11]))
+print(binary_search(5, [2, 3, 5, 7, 11]))
+print(binary_search(3, [2, 3, 5, 7, 11]))
+print(binary_search(11, [2, 3, 5, 7, 11]))
+
+# sample answer
+def binary_search(element, some_list):
+    start_index = 0
+    end_index = len(some_list) - 1
+
+    while start_index <= end_index:
+        midpoint = (start_index + end_index) // 2
+        if some_list[midpoint] == element:
+            return midpoint
+        elif some_list[midpoint] > element:
+            end_index = midpoint - 1
+        else:
+            start_index = midpoint + 1
+    return None
