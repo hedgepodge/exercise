cc2366b3c37ae2d6101a6d77ed87356a77c37546
Hedgepodge <45145360+hedgepodge@users.noreply.github.com>
2020-07-24 10:49:39 +0900

    Update 5. get_data.py

--- data_2020.07/5. get_data.py
+++ data_2020.07/5. get_data.py
@@ -137,3 +137,151 @@ while True:
 print(len(pages))
 
 
+# q
+import requests
+from bs4 import BeautifulSoup
+
+rating_pages = []
+for year in range(2010, 2019):
+    for month in range(1, 13):
+        week = 0
+        while True:
+            page = requests.get('https://workey.codeit.kr/ratings/index?year={0}&month={1}&weekIndex={2}'.format(year, month, week))
+            soup = BeautifulSoup(page.text, 'html.parser')
+            if len(soup.select('.row')) != 1:
+                rating_pages.append(page.text)
+                week += 1
+            else:
+                break
+
+print(len(rating_pages))
+print(rating_pages[0])
+
+# sample answer
+import requests
+from bs4 import BeautifulSoup
+
+# 기간 지정
+years = list(range(2010, 2019))
+months = list(range(1, 13))
+weeks = list(range(0, 5))
+
+# 빈 리스트 생성
+rating_pages = []
+
+for year in years:
+    for month in months:
+        for week in weeks:
+            response = requests.get("https://workey.codeit.kr/ratings/index?year=" + str(year) + "&month=" + str(month) + "&weekIndex=" + str(week))
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            if len(soup.select('.row')) > 1:
+                rating_pages.append(response.text)
+
+
+# webpage to data frame
+import time
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+records = []
+page_num = 1
+
+while True:
+    response = requests.get("http://www.ssg.com/search.ssg?target=all&query=nintendo&page=" + str(page_num))
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # get product info where "prodName" class exists
+    if len(soup.select('.csrch_tip')) == 0:
+        product_names = soup.select('.cunit_info > div.cunit_md.notranslate > div > a > em.tx_ko')
+        product_prices = soup.select('.cunit_info > div.cunit_price.notranslate > div.opt_price > em')
+        product_urls = soup.select('.cunit_prod > div.thmb > a > img')
+        page_num += 1
+        time.sleep(3)
+        
+        for i in range(len(product_names)):
+            record = []
+            record.append(product_names[i].text)
+            record.append(product_prices[i].text.strip())
+            record.append("https://www.ssg.com" + product_urls[i].get('src'))
+            records.append(record)
+    else:
+        break
+
+df = pd.DataFrame(data = records, columns = ["이름", "가격", "이미지 주소"])
+
+df.head()
+
+# q
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+years = list(range(2010, 2019))
+months = list(range(1, 13))
+weeks = list(range(0, 5))
+
+rating_pages = []
+records = []
+
+for year in years:
+    for month in months:
+        for week in weeks:
+            response = requests.get("https://workey.codeit.kr/ratings/index?year=" + str(year) + "&month=" + str(month) + "&weekIndex=" + str(week))
+            soup = BeautifulSoup(response.text, 'html.parser')
+            
+            if len(soup.select('.row')) > 1:
+                rating_pages.append(response.text)
+    
+                period = soup.select('#weekSelectBox [selected="selected"]')[0].text
+                rank = soup.select('.row .rank')[1:]
+                channel = soup.select('.row .channel')[1:]
+                program = soup.select('.row .program')[1:]
+                percent = soup.select('.row .percent')[1:]
+                
+                for i in range(10):
+                    a = []
+                    a.append(period)
+                    a.append(rank[i].text)
+                    a.append(channel[i].text)
+                    a.append(program[i].text)
+                    a.append(percent[i].text)
+                    records.append(a)
+
+df = pd.DataFrame(data=records, columns=['period', 'rank', 'channel', 'program', 'rating'])
+df.head()
+
+# sample answer
+rating_pages = []
+
+for year in years:
+    for month in months:
+        for week in weeks:
+            response = requests.get("https://workey.codeit.kr/ratings/index?year=" + str(year) + "&month=" + str(month) + "&weekIndex=" + str(week))
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            if len(soup.select('.row')) > 1:
+                rating_pages.append(soup)
+
+records = []
+
+for page in rating_pages:
+    date = page.select('option[selected=selected]')[2].text
+    ranks = page.select('.row .rank')[1:]
+    channels = page.select('.row .channel')[1:]
+    programs = page.select('.row .program')[1:]
+    percents = page.select('.row .percent')[1:]
+
+    for i in range(10):
+        record = []
+        record.append(date)
+        record.append(ranks[i].text)
+        record.append(channels[i].text)
+        record.append(programs[i].text)
+        record.append(percents[i].text)
+        records.append(record)
+
+df = pd.DataFrame(data=records, columns=['period', 'rank', 'channel', 'program', 'rating'])
+df.head()
